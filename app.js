9230b353db4a21adc082540f148462a955fc2dc1
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-20 03:39:57 +0530

    CORS error handling by setting response Headers to allow Origin, Methods & Headers

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Header', 'Content-Type, Authorization');
+    next();
+})
 
 app.use('/feeds', feedRoute);
 
